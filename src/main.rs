78c4d94a80f86d22e1c0985a150d879bacc9b09a
Nick Wanninger <nickwanninger@gmail.com>
2020-09-03 10:53:28 -0500

    spawn the node worker in a tokio task

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,29 @@
-use tokio::net::UdpSocket;
+#![feature(trait_alias)]
+
 use std::net;
+use tokio::net::UdpSocket;
 extern crate clap;
-use clap::{Arg, App};
+use clap::{App, Arg};
+use serde::{de::DeserializeOwned, Deserialize, Serialize};
 use std::collections::HashSet;
-use serde::{Serialize, Deserialize, de::DeserializeOwned};
-use tokio::time;
 use std::time::Duration;
-use std::sync::{ Mutex};
-
-
+use tokio::sync::mpsc;
+use tokio::time;
 
 #[derive(Serialize, Deserialize, PartialEq, Debug)]
 enum Operation {
     Broadcast(/* blacklist */ HashSet<net::SocketAddr>),
-    Targetted(/* Target */net::SocketAddr),
+    Targetted(/* Target */ net::SocketAddr),
 }
-
 
 #[derive(Serialize, Deserialize, PartialEq, Debug)]
 enum Payload<T> {
     Heartbeat,
-    Message(T)
+    Message(T),
 }
-
-
 
 #[derive(Serialize, Deserialize, PartialEq, Debug)]
 struct Packet<T>(Operation, Payload<T>);
-
-
-
 
 struct Node<M> {
     sock: UdpSocket,
@@ -38,16 +32,20 @@ struct Node<M> {
     phantom: std::marker::PhantomData<M>,
 }
 
-impl<M: std::marker::Sync + std::marker::Send + Serialize + DeserializeOwned + std::fmt::Debug> Node<M> {
+trait SaneMessage = Send + Sync + Serialize + DeserializeOwned + std::fmt::Debug + 'static;
+
+impl<M: SaneMessage> Node<M> {
     pub async fn new(port: u16) -> Self {
         println!("Listening at 127.0.0.1:{}", port);
-        let sock = UdpSocket::bind((net::Ipv4Addr::new(127, 0, 0, 1), port)).await.unwrap();
+        let sock = UdpSocket::bind((net::Ipv4Addr::new(127, 0, 0, 1), port))
+            .await
+            .unwrap();
         Self {
-            sock, peers: Default::default(),
+            sock,
+            peers: Default::default(),
             phantom: std::marker::PhantomData,
         }
     }
-
 
     pub async fn recv_packet(&mut self) -> (Packet<M>, net::SocketAddr) {
         loop {
@@ -69,21 +67,38 @@ impl<M: std::marker::Sync + std::marker::Send + Serialize + DeserializeOwned + s
         }
     }
 
+    pub async fn start(mut self) -> RunningNode<M> {
+        let (tx, mut metarx) = mpsc::channel::<MetaCommand<M>>(100);
 
-    pub async fn start(&mut self) {
-        let mut heartbeat = time::interval(Duration::from_millis(100));
+        let handle = tokio::spawn(async move {
+            let mut heartbeat = time::interval(Duration::from_millis(100));
 
-        for i in 0.. {
-            tokio::select! {
-                _ = heartbeat.tick() => {
-                    self.broadcast(Payload::Heartbeat).await;
-                },
-                (packet, peer) = self.recv_packet() => {
-                    println!("{} from {}: {:?}", i, peer, packet);
+            let mut active = true;
+            while active {
+                tokio::select! {
+                    _ = heartbeat.tick() => {
+                        self.broadcast(Payload::Heartbeat).await;
+                    },
+                    (packet, peer) = self.recv_packet() => {
+                        println!("msg from {}: {:?}", peer, packet);
+                    },
+                    meta = metarx.recv() => {
+
+                        match meta.unwrap() {
+                            MetaCommand::Die => {
+                                active = false;
+                                println!("Node Terminating");
+                            },
+                            MetaCommand::Broadcast(msg) => {
+                                println!("Told to broadcast '{:?}'", msg);
+                            }
+                        }
+                    },
                 }
             }
-        }
+        });
 
+        return RunningNode { handle, tx };
     }
 
     /// Send a msg to each node in the peer set
@@ -101,35 +116,58 @@ impl<M: std::marker::Sync + std::marker::Send + Serialize + DeserializeOwned + s
     }
 }
 
+enum MetaCommand<M> {
+    Die,
+    Broadcast(M),
+}
 
+struct RunningNode<M> {
+    handle: tokio::task::JoinHandle<()>,
+    tx: mpsc::Sender<MetaCommand<M>>,
+}
 
+impl<M: SaneMessage> RunningNode<M> {
+    pub async fn wait(self) {
+        self.handle.await.unwrap();
+    }
+
+    pub async fn terminate(mut self) {
+        let _ = self.tx.send(MetaCommand::<M>::Die).await;
+        self.wait().await;
+    }
+}
 
 #[tokio::main]
-async fn main()  {
+async fn main() {
+    let matches = App::new("Power over Ethernet Node")
+        .version("1.0")
+        .author("Just a bunch of dumbies")
+        .about("Mesh communication and packet bouncing")
+        .arg(
+            Arg::with_name("port")
+                .short("p")
+                .long("port")
+                .help("Sets a port to listen from")
+                .required(true)
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("peers")
+                .short("c")
+                .long("peers")
+                .help("A CSV list of topologically adjacent servers")
+                .required(true)
+                .takes_value(true),
+        )
+        .get_matches();
 
-     let matches = App::new("Power over Ethernet Node")
-                          .version("1.0")
-                          .author("Just a bunch of dumbies")
-                          .about("Mesh communication and packet bouncing")
-                          .arg(Arg::with_name("port")
-                               .short("p")
-                               .long("port")
-                               .help("Sets a port to listen from")
-                               .required(true)
-                               .takes_value(true))
-                          .arg(Arg::with_name("peers")
-                               .short("c")
-                               .long("peers")
-                               .help("A CSV list of topologically adjacent servers")
-                               .required(true)
-                               .takes_value(true))
-                          .get_matches();
-
-
-    let port = matches.value_of("port").unwrap().parse::<u16>().expect("Port is not an integer");
+    let port = matches
+        .value_of("port")
+        .unwrap()
+        .parse::<u16>()
+        .expect("Port is not an integer");
 
     let mut node: Node<String> = Node::new(port).await;
-
 
     let peer_strings = matches.value_of("peers").unwrap().split(',');
 
@@ -138,6 +176,9 @@ async fn main()  {
         node.add_peer(addr);
     }
 
-    node.start().await;
+    let node = node.start().await;
 
+    // TODO: actually do something
+
+    node.terminate().await;
 }
